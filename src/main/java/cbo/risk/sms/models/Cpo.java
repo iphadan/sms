92659e6d82d5d64d410d82e292ef22c3012bcdb6
iphadan <ephadaniel177@gmail.com>
2025-12-04 06:31:39 +0300

    audit fields has been added to each models

--- src/main/java/cbo/risk/sms/models/Cpo.java
+++ src/main/java/cbo/risk/sms/models/Cpo.java
@@ -3,11 +3,17 @@ package cbo.risk.sms.models;
 import cbo.risk.sms.enums.CheckBookLeaveType;
 import cbo.risk.sms.enums.CheckBookType;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
 @Entity
 @Data
+@Audited
 public class Cpo {
 
     @Id
@@ -20,4 +26,32 @@ public class Cpo {
     @NotBlank
     @Column(nullable = false)
     private int numOfBook;
+
+
+    @NotBlank
+    @Column(name = "BRANCH_ID")
+    private String branchId;
+
+
+    @NotBlank
+    @Column(name = "SUBPROCESS_ID")
+    private String subProcessId;
+    @NotBlank
+    @Column(name = "PROCESS_ID")
+    private String processId;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String createdBy;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String lastUpdatedBy;
+    @Column(name = "CREATED_TS",nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdTimestamp;
+    @UpdateTimestamp
+    @Column(name = "MODIFIED_TS",nullable = false)
+    private LocalDateTime modifiedTimestamp;
+
 }
--- src/main/java/cbo/risk/sms/models/PassBook.java
+++ src/main/java/cbo/risk/sms/models/PassBook.java
@@ -4,12 +4,17 @@ package cbo.risk.sms.models;
 import cbo.risk.sms.enums.PassBookCategory;
 import cbo.risk.sms.enums.PassBookType;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Audited
 public class PassBook {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +33,29 @@ public class PassBook {
     @Column(nullable = false)
     private int numOfBook;
 
+ @NotBlank
+ @Column(name = "BRANCH_ID")
+ private String branchId;
+ @NotBlank
+ @Column(name = "SUBPROCESS_ID")
+ private String subProcessId;
+ @NotBlank
+ @Column(name = "PROCESS_ID")
+ private String processId;
+
+ @NotBlank
+ @Column(nullable = false)
+ private String createdBy;
+
+ @NotBlank
+ @Column(nullable = false)
+ private String lastUpdatedBy;
+ @Column(name = "CREATED_TS",nullable = false)
+ @CreationTimestamp
+ private LocalDateTime createdTimestamp;
+ @UpdateTimestamp
+ @Column(name = "MODIFIED_TS",nullable = false)
+ private LocalDateTime modifiedTimestamp;
 
 
 }
